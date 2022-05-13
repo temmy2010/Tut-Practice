db80199ab2123b21c156d56094ae65b0317e9b1a
TEMIDAYO <akinrotimit@gmail.com>
2022-05-13 02:56:26 -0700

    First push

---
+++ exam.js
@@ -0,0 +1,25 @@
+Number 1: The JavaScript Data type of "Hello" is String.
+
+Number 2: The JavaScript Data type of false is Boolean.
+
+//Number 3: Array of odd numbers between 1 and 20
+let arrOfOddNumbers = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+
+//Number 3: JavaScript program for array of odd numbers between 1 and 20
+let array0fNumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+let odds = array0fNumbers.filter(n => n % 2);
+console.log(odds);
+
+
+//Number 4: Create a variable object that describe student
+let student = {
+  name: "John doe",
+  age: 30,
+  gender: "male",
+  nationality: "Korean",
+  numberOfLanguages: 3,
+  favoriteMusic: ["I shot the Sheriff", "Kokonbilo", "It's a man's world"]
+};
+
+
+Number 5: A group of related components, directives, pipes, and services in Angular is called MODULE.
